1000d81a385ad2731b8a4d6fdc9daa4f27f59cfd
Denis Doan <dedoin08@yahoo.de>
2016-03-07 20:36:51 +0100

    Update Function added

--- js/app.js
+++ js/app.js
@@ -12,24 +12,24 @@ app.config(function($routeProvider){
       .when("/", {
         // with the controller after , it will show the skillsItems
           templateUrl: "views/skillsList.html",
-          controller: "SkillsListItemsController"
+          controller: "HomeController"
       })
     // 12. 2nd route to add the add Item button functionality 
       .when("/inputItem", {
           templateUrl: "views/inputItem.html",
-          controller: "SkillsListItemsController"
+          controller: "SkillsListItemController"
       })
       // 14. routing with id
       .when("/inputItem/edit/:id", {
           templateUrl: "views/inputItem.html",
-          controller: "SkillsListItemsController"
+          controller: "SkillsListItemController"
       })
    // 13. error handling
       .otherwise({
         redirectTo:"/"
         })
 
-})
+});
 
 // 18. Adding a new service
 app.service("SkillsService", function(){
@@ -50,9 +50,10 @@ app.service("SkillsService", function(){
      // Searching for the id
     skillsService.findById = function(id) {
         for (var item in skillsService.skillsItems){
-            if(skillsService.skillsItems[item].id === id)
+            if(skillsService.skillsItems[item].id === id){
               return skillsService.skillsItems[item];
         }
+      }
     };
 
     // 32. Adding a new id function.
@@ -64,7 +65,7 @@ app.service("SkillsService", function(){
        } else {
             // A new and special library underscore.js for arrays and objects is needed for a specific function:max()
             var maxId = _.max(skillsService.skillsItems, function(entry){
-              return entry.id});
+              return entry.id;})
             skillsService.newId = maxId.id +1;
             return skillsService.newId;
             }
@@ -76,20 +77,27 @@ app.service("SkillsService", function(){
 
     // 31. You also have to declare the save function here in the service
     skillsService.save = function(entry){
-      // It gets a new id.
+       
+          //37. update function in the save function
+          var updatedItem = skillsService.findById(entry.id);
+          if (updatedItem) {
+            updatedItem.itemName = entry.itemName;          
+          } else {
+             // It gets a new id.
       entry.id = skillsService.getNewId();
       // Add it into the array
       skillsService.skillsItems.push(entry);
-    }
+          }
+    };
     // 20 return
     return skillsService;
-})
+});
 
 // Controls the body
 // 23 Passing skillsService into this controller
 app.controller("HomeController", ["$scope", "SkillsService", function($scope, SkillsService) {
    // 24 Setting it equal again
-    $scope.appTitle =  SkillsService.skillsItems[0].itemName;
+    $scope.skillsItems =  SkillsService.skillsItems;
     
 
 }]);
@@ -97,23 +105,33 @@ app.controller("HomeController", ["$scope", "SkillsService", function($scope, Sk
 // 15 adding another params $routeParams
 // 21 adding another params SkillsSerivce
 // 27 location service 
-app.controller("SkillsListItemsController", ["$scope", "$routeParams", "$location", "SkillsService", function($scope, $routeParams, $location, SkillsService){
+app.controller("SkillsListItemController", ["$scope", "$routeParams", "$location", "SkillsService", function($scope, $routeParams, $location, SkillsService){
+    
+    //35. Falls kein Id, erstelle ein Id. 
+    if(!$routeParams.id){$scope.skillsItem = {id:0, itemName:""};
+      } else {
+        // 36. Edit. Parseint string into integer -.clone function
+         $scope.skillsItem = _.clone(SkillsService.findById(parseInt($routeParams.id)));
+      }
+
+
     //22 Setting it equal
-    $scope.skillsItems = SkillsService.skillsItems;
+ //   $scope.skillsItems = SkillsService.skillsItems;
     // 28. It is linked to the inputItem.html with ng model. So HTMl5 appears as a placeholder. ng model function declared.
-    $scope.skillsItem = {itemName: "HTML5"}
+ //   $scope.skillsItem = {itemName: "HTML5"} 
 
     // 29. Declaring the save function.
     $scope.save = function(){
+    
       //30. Telling this function to save the skillsItem.
       SkillsService.save($scope.skillsItem);
       $location.path("/");
-    }
+    };
 
     // 16 kind of return statement
-    $scope.rp = "Route Parameter Value: " + $routeParams.id;
+  //  $scope.rp = "Route Parameter Value: " + $routeParams.id;
 
 
     // Debugging by console.logging. In this case, it gives the array with all its objects.
-    console.log($scope.skillsItems);
-}]);
+ //   console.log($scope.skillsItems);
+ }]);
