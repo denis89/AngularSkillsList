f4bd7f8cc0f883d38988b1bd3b59b681054e601a
Denis Doan <dedoin08@yahoo.de>
2016-03-07 10:37:20 +0100

    Adding a save function

--- js/app.js
+++ js/app.js
@@ -36,15 +36,21 @@ app.service("SkillsService", function(){
    var skillsService = {};
    //19 Put the skillsItems here from 15
    skillsService.skillsItems = [
-        {completed: true, itemName: 'HTML5'},
-        {completed: true, itemName: 'CSS3'},
-        {completed: true, itemName: 'jQuery'},
-        {completed: true, itemName: 'Bootstrap'},
-        {completed: true, itemName: 'Responsive Web'},
-        {completed: true, itemName: 'AngularJS'},
-        {completed: true, itemName: 'Web Performance Optimization'},
-        {completed: true, itemName: 'APIs'}
+        {itemName: 'HTML5'},
+        {itemName: 'CSS3'},
+        {itemName: 'jQuery'},
+        {itemName: 'Bootstrap'},
+        {itemName: 'Responsive Web'},
+        {itemName: 'AngularJS'},
+        {itemName: 'Web Performance Optimization'},
+        {itemName: 'APIs'}
     ];
+
+    // 31. You also have to declare the save function here in the service
+    skillsService.save = function(entry){
+      // Add it into the array
+      skillsService.skillsItems.push(entry);
+    }
     // 20 return
     return skillsService;
 })
@@ -60,9 +66,20 @@ app.controller("HomeController", ["$scope", "SkillsService", function($scope, Sk
 
 // 15 adding another params $routeParams
 // 21 adding another params SkillsSerivce
-app.controller("SkillsListItemsController", ["$scope", "$routeParams", "SkillsService", function($scope, $routeParams, SkillsService){
+// 27 location service 
+app.controller("SkillsListItemsController", ["$scope", "$routeParams", "$location", "SkillsService", function($scope, $routeParams, $location, SkillsService){
     //22 Setting it equal
     $scope.skillsItems = SkillsService.skillsItems;
+    // 28. It is linked to the inputItem.html with ng model. So HTMl5 appears as a placeholder. ng model function declared.
+    $scope.skillsItem = {itemName: "HTML5"}
+
+    // 29. Declaring the save function.
+    $scope.save = function(){
+      //30. Telling this function to save the skillsItem.
+      SkillsService.save($scope.skillsItem);
+      $location.path("/");
+    }
+
     // 16 kind of return statement
     $scope.rp = "Route Parameter Value: " + $routeParams.id;
 }]);
