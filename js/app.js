8cf4435350acb17352b6fdf3974c5d242f62c9f5
Denis Doan <dedoin08@yahoo.de>
2016-03-07 13:35:46 +0100

    Adding id function for each skills

--- js/app.js
+++ js/app.js
@@ -36,18 +36,39 @@ app.service("SkillsService", function(){
    var skillsService = {};
    //19 Put the skillsItems here from 15
    skillsService.skillsItems = [
-        {itemName: 'HTML5'},
-        {itemName: 'CSS3'},
-        {itemName: 'jQuery'},
-        {itemName: 'Bootstrap'},
-        {itemName: 'Responsive Web'},
-        {itemName: 'AngularJS'},
-        {itemName: 'Web Performance Optimization'},
-        {itemName: 'APIs'}
+        {id: 1, itemName: 'HTML5'},
+        {id: 2, itemName: 'CSS3'},
+        {id: 3, itemName: 'jQuery'},
+        {id: 4, itemName: 'Bootstrap'},
+        {id: 5, itemName: 'Responsive Web'},
+        {id: 6, itemName: 'AngularJS'},
+        {id: 7, itemName: 'Web Performance Optimization'},
+        {id: 8, itemName: 'APIs'}
     ];
+
+    // 32. Adding a new id function.
+    skillsService.getNewId = function(){
+      if (skillsService.newId) {
+        // Increase the new Id by 1.
+        skillsService.newId++;
+        return skillsService.newId;
+       } else {
+            // A new and special library underscore.js for arrays and objects is needed for a specific function:max()
+            var maxId = _.max(skillsService.skillsItems, function(entry){
+              return entry.id});
+            skillsService.newId = maxId.id +1;
+            return skillsService.newId;
+            }
+
+       };
+
+    
+       
 
     // 31. You also have to declare the save function here in the service
     skillsService.save = function(entry){
+      // It gets a new id.
+      entry.id = skillsService.getNewId();
       // Add it into the array
       skillsService.skillsItems.push(entry);
     }
